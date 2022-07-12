c4edf2eb72dc0233aefaadd63648e1fed888b68c
Dragonruler6008 <Dragonruler6008@gmail.com>
2022-07-11 22:20:57 -0600

    Cleaned up code. Got rid of used import.

--- src/main/java/luke/beacons/BeaconRange.java
+++ src/main/java/luke/beacons/BeaconRange.java
@@ -7,7 +7,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Objects;
 import com.destroystokyo.paper.event.block.BeaconEffectEvent;
